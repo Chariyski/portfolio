824bb17d59e299e7a5b7627382667e61937b67b0
Krasimir Chariyski <kr.chariyski@gmail.com>
2016-04-01 18:31:30 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: "chariyski/configurations/es6-browser"
+};
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# portfolio
+# My Personal Portfolio
+
+## License
+
+[![Apache 2](https://img.shields.io/badge/license-Apache%202-blue.svg)](./LICENSE.txt)
