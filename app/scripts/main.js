68d4c0af69fa710474ca283d71c0fa2c04373975
Krasimir Chariyski <kr.chariyski@gmail.com>
2016-04-29 14:14:33 +0300

    Add smooth scrolling and gumshoe to navigation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,6 @@
 import navigation from './modules/navigation';
+import smoothScroll from 'smooth-scroll';
+import gumshoe from 'gumshoe';
 
 const sidebarOpenButton = document.getElementById('open-sidebar'),
   sidebarCloseButton = document.getElementById('close-sidebar');
@@ -6,3 +8,12 @@ const sidebarOpenButton = document.getElementById('open-sidebar'),
 sidebarOpenButton.addEventListener('click', navigation.toggle, false);
 sidebarCloseButton.addEventListener('click', navigation.toggle, false);
 
+smoothScroll.init({
+  speed: 800,
+  easing: 'easeInOutCubic'
+});
+
+gumshoe.init({
+  activeClass: 'navigation__item--active',
+  offset: 20
+});
