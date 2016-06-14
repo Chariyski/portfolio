e6552ce09dadc729227c1a93cb72aff66a93c9b7
Krasimir Chariyski <kr.chariyski@gmail.com>
2016-06-14 13:48:42 +0300

    Add mail sending through Formspree API

--- app/scripts/components/Contact-form.js
+++ app/scripts/components/Contact-form.js
@@ -14,12 +14,14 @@ class ContactForm {
    */
   constructor(elementId) {
     this._contactForm = document.getElementById(elementId);
+    this._contactFormSubmitButton = this._contactForm.querySelector('[data-contact-form="submit"]');
+    this._crateContactFormMessageContainer();
     this._initialiseEventHandlers();
   }
 
   /**
    * Validate field input.
-   * @param {element} element - input or textarea DOM element.
+   * @param {Element} element - input or textarea DOM element.
    * @returns {boolean}
    */
   isFieldValid(element) {
@@ -46,12 +48,26 @@ class ContactForm {
   }
 
   /**
+   * Create container for state massages.
+   * @private
+   * @returns {Element} Reference to the message container
+   */
+  _crateContactFormMessageContainer() {
+    const messageContainer = document.createElement('p');
+
+    messageContainer.setAttribute('data-contact-form', 'message');
+    this._contactForm.appendChild(messageContainer);
+
+    this._contactFormMessageContainer = messageContainer;
+  }
+
+  /**
    * Attach event handlers.
    * @private
    * @returns {undefined}
    */
   _initialiseEventHandlers() {
-    this._contactForm.addEventListener('submit', this._onSubmit.bind(this), false);
+    this._contactForm.addEventListener('submit', this._formSubmitHandler.bind(this), false);
     this._contactForm.addEventListener('blur', this._visualFiledValidation.bind(this), true);
   }
 
@@ -74,18 +90,78 @@ class ContactForm {
   }
 
   /**
-   * Handler for form submit event.
+   * Send mail through Formspee API.
+   * @private
+   * @returns {undefined}
+   */
+  _sendMailWithFormspree() {
+    const xhttp = new XMLHttpRequest();
+    const messageData = {
+      subject: this._contactForm.subject,
+      name: this._contactForm.name,
+      email: this._contactForm.email,
+      message: this._contactForm.message,
+      _gotcha: this._contactForm.querySelector('[data-contact-form="gotcha"]').value
+    };
+
+    xhttp.open('POST', 'https://formspree.io/kr.chariyski@gmail.com', true);
+    xhttp.setRequestHeader('Content-type', 'application/json');
+
+    xhttp.addEventListener('progress', this._formProgressHandler.bind(this), false);
+    xhttp.addEventListener('load', this._formLoadHandler.bind(this, xhttp), false);
+    xhttp.addEventListener('error', this._formErrorHandler.bind(this), false);
+
+    xhttp.send(JSON.stringify(messageData));
+  }
+
+  /**
+   * Progress event handler.
+   * @private
+   * @returns {undefined}
+   */
+  _formProgressHandler() {
+    this._contactFormSubmitButton.setAttribute('disabled', true);
+  }
+
+  /**
+   * Load event handler.
+   * @param {object} xhttp - XMLHttpRequest object.
+   * @private
+   * @returns {undefined}
+   */
+  _formLoadHandler(xhttp) {
+    const response = JSON.parse(xhttp.response);
+
+    if (response.success) {
+      this._contactFormSubmitButton.parentNode.removeChild(this._contactFormSubmitButton);
+      this._contactFormMessageContainer.innerText = 'The message was send successful.';
+    }
+  }
+
+  /**
+   * Error event handler.
+   * @param {object} xhttp - XMLHttpRequest object.
+   * @private
+   * @returns {undefined}
+   */
+  _formErrorHandler(xhttp) {
+    console.log(xhttp);
+
+    // TODO add offline response
+    this._contactFormMessageContainer.innerText = 'Something went wrong, please try again later.';
+  }
+
+  /**
+   * Submit event handler.
    * @param {Object} event - Submit event
    * @private
    * @returns {undefined}
    */
-  _onSubmit(event) {
+  _formSubmitHandler(event) {
     event.preventDefault();
 
-    if (this.isFormValid(event.target)) {
-
-      // Ajax call.
-
+    if (this.isFormValid(this._contactForm)) {
+      this._sendMailWithFormspree();
     }
   }
 
