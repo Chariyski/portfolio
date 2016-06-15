c54a69f5e93991cb9f17696141153b9e8417ab61
Krasimir Chariyski <kr.chariyski@gmail.com>
2016-06-15 13:52:43 +0300

    Add criticalcss Refactor production build

---
+++ build.production.config.js
@@ -0,0 +1,28 @@
+var Version = require('node-version-assets');
+var criticalCSS = require('critical');
+
+var versionInstance = new Version({
+  assets: ['build/styles/main.css', 'build/scripts/bundle.js'],
+  grepFiles: ['build/index.html']
+});
+
+// Inline critical CSS
+criticalCSS.generate({
+  inline: true,
+  base: 'build/',
+  src: 'index.html',
+  dest: 'build/index.html',
+  dimensions: [{
+    height: 640,
+    width: 360
+  }, {
+    height: 1080,
+    width: 1920
+  }],
+  minify: true
+}).then(function (output) {
+  // Create files version
+  versionInstance.run();
+}).error(function (err) {
+  console.log(err);
+});
--- file-versioning.js
+++
@@ -1,8 +0,0 @@
-var Version = require('node-version-assets');
-
-var versionInstance = new Version({
-  assets: ['build/styles/main.css', 'build/scripts/bundle.js'],
-  grepFiles: ['build/index.html']
-});
-
-versionInstance.run();
